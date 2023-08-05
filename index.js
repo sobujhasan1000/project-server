d36544dcb56d5ce18830fcae6a3a4e795a05b4ea
mshipan <mshipan657@gmail.com>
2023-08-05 13:52:12 +0600

    test api created

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
-require('dotenv').config()
-const port=process.env.PORT || 5000;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zunrmyl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,15 +27,19 @@ async function run() {
 
     // ======================== all collection here ==========================
 
-    const projectCollection=client.db('projecttest').collection('info');
+    const projectCollection = client.db("projecttest").collection("info");
 
     // ========================== get post update modify here ==================
-
-
+    app.get("/info", async (req, res) => {
+      const result = await projectCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -44,11 +47,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("project test runing");
+});
 
-app.get('/', (req,res)=>{
-    res.send('project test runing')
-})
-
-app.listen(port, ()=>{
-    console.log(`project is runing on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`project is runing on port ${port}`);
+});
